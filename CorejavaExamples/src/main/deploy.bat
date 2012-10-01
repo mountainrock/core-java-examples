aaf1e63b5b75584a6425f9beceb0208363ea8943
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@7096bdd4-2500-0fdb-920d-270ca97c0f7c>
2012-10-01 20:00:30 +0000

    e

--- CorejavaExamples/src/main/deploy.bat
+++ CorejavaExamples/src/main/deploy.bat
@@ -3,5 +3,5 @@ rmdir /s /q %CATALINA_HOME%\webapps\coacs
 
 cmd /c mvn clean install
 cmd /c %CATALINA_HOME%\bin\shutdown.bat
-cmd /c copy target/*.war %CATALINA_HOME%\webapps
+cmd /c copy target\*.war %CATALINA_HOME%\webapps
 cmd /c %CATALINA_HOME%\bin\startup.bat
