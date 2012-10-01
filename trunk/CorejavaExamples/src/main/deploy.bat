a4c6ccf0c5091f8dd35a6c0057fdf95a26d6b493
sandeep.maloth@gmail.com <sandeep.maloth@gmail.com@7096bdd4-2500-0fdb-920d-270ca97c0f7c>
2012-10-01 19:41:04 +0000

    deploy

---
+++ trunk/CorejavaExamples/src/main/deploy.bat
@@ -0,0 +1,7 @@
+set CATALINA_HOME=D:\Sandeep\tools\Servers\tomcat\apache-tomcat-6.0.24
+rmdir /s /q %CATALINA_HOME%\webapps\coacs
+
+cmd /c mvn clean install
+cmd /c %CATALINA_HOME%\bin\shutdown.bat
+cmd /c copy target/*.war %CATALINA_HOME%\webapps
+cmd /c %CATALINA_HOME%\bin\startup.bat
